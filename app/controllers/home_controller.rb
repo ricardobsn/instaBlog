ef862b6e0a1df99decd784ee3571c059eb3c2e0b
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-03-16 17:10:19 -0300

    listagem dos posts OK

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @postagems = Postagem.all
   end
 end
--- app/models/postagem.rb
+++ app/models/postagem.rb
@@ -1,4 +1,9 @@
 class Postagem < ApplicationRecord
 validates_presence_of :titulo, :conteudo
 validates_length_of :conteudo, minimum: 10, allow_blank: false
+
+# def complete_name
+#     "#{titulo}, #{conteudo}"
+#   end
+
 end
