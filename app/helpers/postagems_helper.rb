2096441fd2da30cc8f2b89cf411bf5b4da452f45
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-03-17 12:08:10 -0300

    melhora layout

--- app/helpers/postagems_helper.rb
+++ app/helpers/postagems_helper.rb
@@ -1,6 +1,6 @@
 module PostagemsHelper
   def post_date(date)
     # formatting date: Aug, 31 2007 - 9:55PM
-    date.strftime("posted on %b, %m %Y - %H:%M")
+    date.strftime("postado em %b, %m %Y - %H:%M")
   end
 end
