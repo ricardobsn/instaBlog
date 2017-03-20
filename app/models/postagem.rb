95d510fca9b66a765a2cb3a05b2ff003f8b056b2
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-03-20 08:52:25 -0300

    mudança imagem de fundo e inversão da orde dos comentarios no index

--- app/models/postagem.rb
+++ app/models/postagem.rb
@@ -2,6 +2,7 @@ class Postagem < ApplicationRecord
 validates_presence_of :titulo, :conteudo
 validates_length_of :conteudo, minimum: 10, allow_blank: false
 
+
 # def complete_name
 #     "#{titulo}, #{conteudo}"
 #   end
